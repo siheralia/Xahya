73a5d6a573d35269aa00187cacd4fc82147031f8
siheralia <arisihel@gmail.com>
2026-09-19 02:22:25 -0600

    Configura Prisma 8

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { definePrismaConfig } from "prisma/config";
+import { defineConfig as ormConfig } from "@prisma/orm-postgres/config";
+
+export default definePrismaConfig({
+  orm: ormConfig({
+    contract: "./prisma/contract.prisma",
+  }),
+});
